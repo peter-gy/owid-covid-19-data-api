e45e780b233979408b0d7554d8184b80b1429659
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-01-09 20:08:45 +0100

    feat: disable CORS, .gitignore pyenv file

--- owid_covid19_data_api/main.py
+++ owid_covid19_data_api/main.py
@@ -1,4 +1,12 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_csv import FastAPI_CSV
 
 csv_url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'
 app = FastAPI_CSV(csv_url)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
